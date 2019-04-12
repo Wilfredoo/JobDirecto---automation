915a879af14b816953d2a4816f3ec6201f222b46
Wilfredo <casas.farach@gmail.com>
2019-04-12 20:43:58 +0200

    Now the emails are saved to the RESULT.csv

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -5,13 +5,16 @@ end
 Given("I save all emails") do
   i = 1
 
-  120.times do
+  3.times do
     find(:xpath, "(//a[@class='result-title hdrlnk'])[#{i}]").click
     if Capybara.has_xpath?("//button[@class='reply-button js-only']")
       find(:xpath, "//button[@class='reply-button js-only']").click
       sleep(1)
       if Capybara.has_xpath?("//p[@class='reply-email-address']")
-        puts find(:xpath, "//p[@class='reply-email-address']//a").text
+        # puts find(:xpath, "//p[@class='reply-email-address']//a").text
+        open('RESULTS.csv', 'a') do |f|
+          f << find(:xpath, "//p[@class='reply-email-address']//a").text + "\n"
+        end
       end
     end
     step %{I go to "https://newyork.craigslist.org/search/fbh"}
