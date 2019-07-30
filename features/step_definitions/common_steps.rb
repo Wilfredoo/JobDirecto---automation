387386e219151c994094e1b9028236f78fc3db82
Wilfredo <casas.farach@gmail.com>
2019-07-30 13:13:35 +0200

    a

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -4,20 +4,32 @@ end
 
 Given("I save all emails") do
   i = 1
+  j = 0
 
-  120.times do
-    find(:xpath, "(//a[@class='result-title hdrlnk'])[#{i}]").click
+  240.times do
+    if i <= 120
+      find(:xpath, "(//a[@class='result-title hdrlnk'])[#{i}]").click
+    else
+      find(:xpath, "(//a[@class='result-title hdrlnk'])[#{j}]").click
+    end
+
     if Capybara.has_xpath?("//button[@class='reply-button js-only']")
       find(:xpath, "//button[@class='reply-button js-only']").click
       sleep(1)
       if Capybara.has_xpath?("//p[@class='reply-email-address']")
-        # puts find(:xpath, "//p[@class='reply-email-address']//a").text
         open('RESULTS.csv', 'a') do |f|
           f << find(:xpath, "//p[@class='reply-email-address']//a").text + "\n"
         end
       end
     end
-    step %{I go to "https://newyork.craigslist.org/search/fbh"}
     i += 1
+    if i >= 120
+      j += 1
+      step %{I go to "https://newyork.craigslist.org/search/spa?s=120"}
+    else
+      step %{I go to "https://newyork.craigslist.org/search/spa"}
+    end
   end
 end
+
+# trd - fbh - lab - spa - rfh
--- features/tests/craigslist.feature
+++ features/tests/craigslist.feature
@@ -1,5 +1,7 @@
 Feature: Open multiple tabs in Craigslist
 
     Scenario: To save time I want to open all the tabs automatically
-      Given I go to "https://newyork.craigslist.org/search/fbh"
-      And I save all emails
+      #Given I go to "https://newyork.craigslist.org/search/fbh"
+Given I go to "https://newyork.craigslist.org/search/trd"
+            And I save all emails
+        #    Given I go to "https://newyork.craigslist.org/search/lab"
